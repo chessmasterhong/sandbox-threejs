70d7e3cbd944ded8130e48e7983b78f7190af66d
Kevin Chan <chessmasterhong@yahoo.com>
2014-09-21 19:00:42 -0400

    Converted main.js into function form. Added strict mode.

--- scripts/game/main.js
+++ scripts/game/main.js
@@ -1,24 +1,30 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+/* global THREE */
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+(function() {
+    'use strict';
 
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-camera.position.z = 5;
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-var render = function () {
-    requestAnimationFrame(render);
+    var geometry = new THREE.BoxGeometry(1,1,1);
+    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+    camera.position.z = 5;
 
-    renderer.render(scene, camera);
-};
+    var render = function () {
+        requestAnimationFrame(render);
 
-render();
+        cube.rotation.x += 0.1;
+        cube.rotation.y += 0.1;
+
+        renderer.render(scene, camera);
+    };
+
+    render();
+})();
